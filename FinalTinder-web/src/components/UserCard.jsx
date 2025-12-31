b3deb894f4cc00f0c9e2c65c70ef0b62a2a4e10d
Vamshi Krishna <yenaganti.vamshi007@gmail.com>
2025-12-31 17:05:57 +0530

    Refactor axios usage in UserCard component

--- FinalTinder-web/src/components/UserCard.jsx
+++ FinalTinder-web/src/components/UserCard.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
+import axiosInstance from "../utils/axios";
 import { useDispatch } from "react-redux";
 import { removeUserFromFeed } from "../utils/feedSlice";
 
@@ -9,7 +10,7 @@ const UserCard = ({ user }) => {
 
   const handleSendRequest = async (status, userId) => {
     try {
-      const res = await axios.post(
+      const res = await axiosInstance.post(
         BASE_URL + "/request/send/" + status + "/" + userId,
         {},
         { withCredentials: true }
