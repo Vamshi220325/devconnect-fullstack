373b918ed94766c8f00916314911765b452a14cf
Vamshi Krishna <yenaganti.vamshi007@gmail.com>
2025-12-31 14:37:48 +0530

    Update app.js

--- FinalDevBack/src/app.js
+++ FinalDevBack/src/app.js
@@ -11,9 +11,9 @@ const http = require("http");
 app.use(
   cors({
     origin: [
-      "https://devconnect-fullstack-2zvr.onrender.com", // Your live frontend URL
-      "http://localhost:5173",                         // Standard Vite local port
-      "http://localhost:3000"                          // Standard React local port
+      "https://devconnect-fullstack-2zvr.onrender.com", 
+      "http://localhost:5173",                         
+      "http://localhost:3000"                          
     ],
     credentials: true,
   })
