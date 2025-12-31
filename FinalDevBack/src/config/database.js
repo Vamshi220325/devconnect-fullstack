04bda72866ddada80103eb17bb3a915a23ecd778
Vamshi Krishna <yenaganti.vamshi007@gmail.com>
2025-12-31 14:42:50 +0530

    Update database.js

--- FinalDevBack/src/config/database.js
+++ FinalDevBack/src/config/database.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
-
 const connectDB = async () => {
   try {
-    // This looks for the variable you will create in Render
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected successfully to the cloud!");
+    console.log("MongoDB connected successfully!");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
-    process.exit(1); // Stop the server if the database fails
+    process.exit(1);
   }
 };
-
 module.exports = connectDB;
