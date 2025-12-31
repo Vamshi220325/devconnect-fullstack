7d413a020cea79362cbddb2ccdcfbc86519227ed
Vamshi Krishna <yenaganti.vamshi007@gmail.com>
2025-12-31 15:09:09 +0530

    Update constants.js

--- FinalTinder-web/src/utils/constants.js
+++ FinalTinder-web/src/utils/constants.js
@@ -1,2 +1,2 @@
 export const USER_ICON="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe7qKgRvChw4p7QLmLJ_Vw2PyM11C6ThI6oA&s";
-export const BASE_URL="http://localhost:3001";
+export const BASE_URL="https://devconnect-fullstack-864c.onrender.com";
