d2dd8f1899e3f0df1b0b7fdb21a8f27ac42d22ba
Vamshi Krishna <yenaganti.vamshi007@gmail.com>
2025-12-31 15:37:11 +0530

    Update auth.js

--- FinalDevBack/src/routes/auth.js
+++ FinalDevBack/src/routes/auth.js
@@ -29,6 +29,8 @@ authRouter.post("/signup", async (req, res) => {
 
     res.cookie("token", token, {
       expires: new Date(Date.now() + 8 * 3600000),
+      httpOnly: true,    
+    secure: true,
     });
 
     res.json({ message: "User Added successfully!", data: savedUser });
@@ -52,6 +54,8 @@ authRouter.post("/login", async (req, res) => {
 
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
+        httpOnly: true,    
+    secure: true,
       });
       res.send(user);
     } else {
